f71fac8280f21e3219fecaeb4ce1cec4f6c53d2e
QUOTRIX <solacecryptocurrency@gmail.com>
2025-02-11 13:17:17 +0100

    Move search template to templates directory

---
+++ wp-content/themes/solve/templates/search-template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Search Page
+ * Template Post Type: page
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <div id="app-search"></div>
+    </main>
+</div>
+
+<?php
+get_footer();
+?>
