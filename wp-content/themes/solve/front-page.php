cd67e9580ae0062b644fa63d9a665c0330d16792
QUOTRIX <solacecryptocurrency@gmail.com>
2025-02-11 13:20:55 +0100

    Add front-page template

---
+++ wp-content/themes/solve/front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The front page template file
+ */
+
+get_header();
+?>
+
+<div class="content-area">
+    <main class="site-main">
+        <div id="app-search" class="search-container"></div>
+    </main>
+</div>
+
+<?php
+get_footer();
+?>
