3c71adbf061d235372e83ca45bf9bfd88ecdda1f
QUOTRIX <solacecryptocurrency@gmail.com>
2025-02-11 13:25:15 +0100

    Update front-page.php with centered search bar

--- wp-content/themes/solve/front-page.php
+++ wp-content/themes/solve/front-page.php
@@ -1,17 +1,10 @@
-<?php
-/**
- * The front page template file
- */
+<?php get_header(); ?>
 
-get_header();
-?>
-
-<div class="content-area">
-    <main class="site-main">
-        <div id="app-search" class="search-container"></div>
-    </main>
+<div class="content-area" style="min-height: 100vh; display: flex; align-items: center; justify-content: center; background-color: #f5f5f5;">
+    <div style="width: 100%; max-width: 800px; padding: 20px;">
+        <h1 style="text-align: center; margin-bottom: 30px; font-size: 2.5em;">Find Your Apps</h1>
+        <div id="app-search"></div>
+    </div>
 </div>
 
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
