4c3d355d5d280b2bb71dc4a6310405c6a5f11e50
QUOTRIX <solacecryptocurrency@gmail.com>
2025-02-11 13:31:08 +0100

    Create proper WordPress theme structure

--- wp-content/themes/solve/functions.php
+++ wp-content/themes/solve/functions.php
@@ -1,40 +1,7 @@
 <?php
-
 function solve_enqueue_scripts() {
-    // Enqueue React
     wp_enqueue_script('react', 'https://unpkg.com/react@17/umd/react.development.js', array(), '17.0.0', true);
     wp_enqueue_script('react-dom', 'https://unpkg.com/react-dom@17/umd/react-dom.development.js', array('react'), '17.0.0', true);
-    
-    // Enqueue the search component
-    wp_enqueue_script('search-component', get_template_directory_uri() . '/components/SearchBar.js', array('react', 'react-dom'), '1.0.0', true);
-    
-    // Enqueue styles
-    wp_enqueue_style('search-styles', get_template_directory_uri() . '/styles/search-bar.css', array(), '1.0.0');
-    
-    // Add inline initialization script
-    wp_add_inline_script('search-component', '
-        document.addEventListener("DOMContentLoaded", function() {
-            const searchContainer = document.getElementById("app-search");
-            if (searchContainer && typeof SearchBar !== "undefined") {
-                ReactDOM.render(React.createElement(SearchBar), searchContainer);
-            }
-        });
-    ');
+    wp_enqueue_script('search-component', get_template_directory_uri() . '/js/search.js', array('react', 'react-dom'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'solve_enqueue_scripts');
-
-// Log errors to debug
-function solve_add_error_logging() {
-    if (!is_admin()) {
-        echo '<script>
-            console.log("Page loaded");
-            window.onerror = function(msg, url, lineNo, columnNo, error) {
-                console.log("Error: " + msg + "\nURL: " + url + "\nLine: " + lineNo + "\nColumn: " + columnNo + "\nError object: " + JSON.stringify(error));
-                return false;
-            };
-        </script>';
-    }
-}
-add_action('wp_head', 'solve_add_error_logging');
-
-?>
--- wp-content/themes/solve/index.php
+++ wp-content/themes/solve/index.php
@@ -1,42 +1,8 @@
-<?php
-/**
- * The main template file
- */
-
-get_header(); ?>
-
-<div class="search-wrapper" style="
-    min-height: 80vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #f5f5f5;
-    padding: 20px;
-">
-    <div style="
-        width: 100%;
-        max-width: 600px;
-        text-align: center;
-    ">
-        <h1 style="
-            font-size: 2.5em;
-            margin-bottom: 30px;
-            color: #333;
-        ">Find Your Apps</h1>
-        
+<?php get_header(); ?>
+<div class="search-wrapper">
+    <div class="search-container">
+        <h1>Find Your Apps</h1>
         <div id="app-search"></div>
-        
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                console.log('Content loaded, checking React...');
-                if (typeof React !== 'undefined' && typeof ReactDOM !== 'undefined') {
-                    console.log('React is loaded');
-                } else {
-                    console.log('React is not loaded');
-                }
-            });
-        </script>
     </div>
 </div>
-
 <?php get_footer(); ?>
